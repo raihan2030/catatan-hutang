ab26bca599d0b779792fdcfa5c1e9332cc03ac16
raihan2030 <mr799903@gmail.com>
2025-06-30 17:52:44 +0800

    test commit

--- catatan_hutang/settings.py
+++ catatan_hutang/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'), # Menggunakan service name 'db' (memenuhi syarat #5)
+        'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
     }
     # 'default': {
